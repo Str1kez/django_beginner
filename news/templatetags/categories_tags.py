30b16e69594e300cc479ae1c45440bb952d63369
Str1kez <vazinov@yandex.ru>
2022-01-11 00:15:37 +0300

    dropdown menu was updated

--- news/templatetags/categories_tags.py
+++ news/templatetags/categories_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from news.models import Category
 
@@ -7,7 +8,7 @@ register = template.Library()
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(article_count=Count('article')).filter(article_count__gt=0)
 
 # @register.inclusion_tag('news/dropdown_menu.html')
 # def get_dropdown_menu():
