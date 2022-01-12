69da5a239476e7770856ac4f3be60bd76d4401f1
Str1kez <vazinov@yandex.ru>
2022-01-13 00:07:22 +0300

    admin's page customization

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Article, Category
 
 # Register your models here.
@@ -6,11 +8,20 @@ from .models import Article, Category
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'author', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'author', 'created_at', 'updated_at', 'is_published', 'get_image')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'author')  # регистр не имеет значения только на англ
     list_filter = ('category', 'is_published')
     list_editable = ('is_published', )
+    actions_on_top = True
+    fields = ('title', 'category', 'author', 'text', 'is_published', 'image', 'get_image', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at', 'get_image')
+
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" alt="Не прогрузилась" width=75>')
+
+    get_image.short_description = 'Картинка'
 
 
 admin.site.register(Article, ArticleAdmin)
